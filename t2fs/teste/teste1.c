5ca219693e8359478b09b6dec657984a8df47103
EduardoRenani00 <eduardo.renani@hotmail.com>
2018-11-11 15:12:56 -0200

    criacao do framework de teste para terceiros

---
+++ t2fs/teste/teste1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/t2fs.h"
+#include "../include/disk.h"
+#include "../include/utils.h"
+
+//criar o testebench chamando as funcoes da t2fs.h
+
+int main(){
+
+    return 0;
+}
+
